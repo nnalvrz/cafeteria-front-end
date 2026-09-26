3a27acc0d99aa508c5c656631e176dac932cd290
nnalvrz <ambarverenisen@gmail.com>
2026-09-26 12:28:30 -0600

    fix: importar api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-// Si existe la variable de entorno la usa, de lo contrario apunta directamente al backend de Render
-const API_URL = import.meta.env.VITE_API_URL || 'https://cafeteria-back-end.onrender.com';
-
 export const api = axios.create({
-  baseURL: API_URL
+    baseURL: 'https://cafeteria-back-end.onrender.com' // Reemplaza con tu URL real de Render
 });
